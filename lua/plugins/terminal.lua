263fe5aaa022c6e8ec1c92794b287d5208f044bd
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-12 13:48:50 -0300

    Disable spelling on terminal buffer

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,7 +2,13 @@ return {
   {
     'akinsho/toggleterm.nvim',
     version = "*",
-    config = true,
+    config = function ()
+      require("toggleterm").setup({
+        on_open = function ()
+          vim.cmd("setlocal nospell")
+        end
+      })
+    end,
     keys = {
       { "<C-T>", "<cmd>ToggleTerm name=main suze=20<CR>" }
     }
