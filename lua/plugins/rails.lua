e267f13e886c563493ae0f7ef4fcf7ff1be5c359
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-14 22:59:34 -0300

    Automatically set eyaml files to yaml to get nice syntax highlighting

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -1,8 +1,13 @@
 return {
   {
     "tpope/vim-rails",
-    config = function ()
+    config = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "eruby.yaml",
+        command = "set filetype=yaml",
+      })
+
       vim.keymap.set({ "n", "v" }, "<C-A>", "<cmd>A<CR>", { desc = "Alternate between rails files" })
-    end
-  }
+    end,
+  },
 }
