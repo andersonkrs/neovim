cb307ac2afbc149c1c7e740325893a3cc0262f67
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-14 22:59:10 -0300

    Improve Trouble mappings

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -1,25 +1,30 @@
 return {
-  {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      use_diagnostic_signs = true,
-    },
-    keys = {
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>",  desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>",               desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>",              desc = "Quickfix List (Trouble)" },
-    },
-    config = function()
-      vim.keymap.set({ "n", "v" }, "<leader>xn",
-        function()
-          local trouble = require("trouble")
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			use_diagnostic_signs = true,
+		},
+		keys = {
+			{ "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
+			{ "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
+			{ "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
+			{ "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
+		},
+		config = function()
+			vim.keymap.set({ "n", "v" }, "<leader>x]", function()
+				local trouble = require("trouble")
 
-          trouble.open()
-          trouble.next({ skip_groups = true, jump = true });
-        end,
-        { desc = "Next diagnostic (Trouble)" })
-    end
-  }
+				trouble.open()
+				trouble.next({ skip_groups = true, jump = true })
+			end, { desc = "Next diagnostic (Trouble)" })
+
+			vim.keymap.set({ "n", "v" }, "<leader>x[", function()
+				local trouble = require("trouble")
+
+				trouble.open()
+				trouble.previous({ skip_groups = true, jump = true })
+			end, { desc = "Previous diagnostic (Trouble)" })
+		end,
+	},
 }
