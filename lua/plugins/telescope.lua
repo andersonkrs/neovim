a93577db1dc3bf1eee01657544e769e2695be5d6
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-14 23:13:29 -0300

    Reformat telescope configs

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,40 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local telescope = require('telescope')
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local telescope = require("telescope")
 
-      telescope.setup({
-        pickers = {
-          find_files = {
-            hidden = true
-          }
-        }
-      })
+			telescope.setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+			})
 
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<C-k>", builtin.live_grep, {})
-      vim.keymap.set("n", "<C-h>", "<Cmd>Telescope oldfiles<CR>")
-      vim.keymap.set("n", "<C-s>", builtin.lsp_document_symbols, {})
-    end,
-  },
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({
-              -- even more opts
-            }),
-          },
-        },
-      })
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+			vim.keymap.set("n", "<C-k>", builtin.live_grep, {})
+			vim.keymap.set("n", "<C-h>", "<Cmd>Telescope oldfiles<CR>", { desc = "Telescope Recent Files" })
+			vim.keymap.set("n", "<C-s>", builtin.lsp_document_symbols, { desc = "Telescope Document Symbols" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({
+							-- even more opts
+						}),
+					},
+				},
+			})
 
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
