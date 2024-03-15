c2eef6ce4b976206f3fe90a55efcef195498392a
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-14 23:13:09 -0300

    Lots of syntax plugins

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -1,94 +1,132 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
-            node_incremental = "<Leader>ss",
-            scope_incremental = "<Leader>sc",
-            node_decremental = "<Leader>sd",
-          },
-        },
-        textobjects = {
-          move = {
-            enable = true,
-            goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-            goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-            goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-            goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
-          },
-        },
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-  },
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    event =  { "BufReadPost", "BufWritePost", "BufNewFile" },
-    enabled = true,
-    opts = { mode = "cursor", max_lines = 3 },
-    keys = {
-      {
-        "<leader>ut",
-        function()
-          local Util = require("lazyvim.util")
-          local tsc = require("treesitter-context")
-          tsc.toggle()
-          if Util.inject.get_upvalue(tsc.toggle, "enabled") then
-            Util.info("Enabled Treesitter Context", { title = "Option" })
-          else
-            Util.warn("Disabled Treesitter Context", { title = "Option" })
-          end
-        end,
-        desc = "Toggle Treesitter Context",
-      },
-    },
-  },
-  {
-    "RRethy/vim-illuminate",
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    opts = {
-      delay = 200,
-      large_file_cutoff = 2000,
-      large_file_overrides = {
-        providers = { "lsp" },
-      },
-    },
-    config = function(_, opts)
-      require("illuminate").configure(opts)
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				auto_install = true,
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
+						node_incremental = "<Leader>ss",
+						scope_incremental = "<Leader>sc",
+						node_decremental = "<Leader>sd",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
 
-      local function map(key, dir, buffer)
-        vim.keymap.set("n", key, function()
-          require("illuminate")["goto_" .. dir .. "_reference"](false)
-        end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-      end
+						-- Automatically jump forward to textobj, similar to targets.vim
+						lookahead = true,
 
-      map("]]", "next")
-      map("[[", "prev")
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+						},
+					},
+					move = {
+						enable = true,
+						goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+						goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+						goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+						goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+					},
+				},
+				matchup = { enable = true },
+			})
 
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      vim.api.nvim_create_autocmd("FileType", {
-        callback = function()
-          local buffer = vim.api.nvim_get_current_buf()
-          map("]]", "next", buffer)
-          map("[[", "prev", buffer)
-        end,
-      })
-    end,
-    keys = {
-      { "]]", desc = "Next Reference" },
-      { "[[", desc = "Prev Reference" },
-    },
-  }
+			-- Add support for ERB
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+			parser_config.erb = {
+				install_info = {
+					url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+					files = { "src/parser.c" },
+					requires_generate_from_grammar = true,
+				},
+				filetype = "erb",
+			}
+			parser_config.ejs = {
+				install_info = {
+					url = "https://github.com/tree-sitter/tree-sitter-embedded-template",
+					files = { "src/parser.c" },
+					requires_generate_from_grammar = true,
+				},
+				filetype = "ejs",
+			}
+		end,
+
+		vim.keymap.set("n", "[x", function()
+			require("treesitter-context").go_to_context(vim.v.count1)
+		end, { silent = true, desc = "Jump to the most outer context" }),
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		enabled = true,
+		opts = { mode = "cursor", max_lines = 3 },
+	},
+	-- Quickly swap surrounding elements
+	{ "tpope/vim-surround" },
+	-- Quickly switch between single-line and multi-line elements
+	{
+		"AndrewRadev/splitjoin.vim",
+		config = function()
+			vim.keymap.set("n", "<leader>gj", "<cmd>SplitjoinJoin<CR>", { desc = "Join in a single line" })
+			vim.keymap.set("n", "<leader>gs", "<cmd>SplitjoinSplit<CR>", { desc = "Split in a single line" })
+		end,
+	},
+	-- Navigate language elements using %
+	{
+		"andymass/vim-matchup",
+		config = function()
+			vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		end,
+	},
+	{
+		"RRethy/vim-illuminate",
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		opts = {
+			delay = 200,
+			large_file_cutoff = 2000,
+			large_file_overrides = {
+				providers = { "lsp" },
+			},
+		},
+		config = function(_, opts)
+			require("illuminate").configure(opts)
+
+			local function map(key, dir, buffer)
+				vim.keymap.set("n", key, function()
+					require("illuminate")["goto_" .. dir .. "_reference"](false)
+				end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+			end
+
+			map("]]", "next")
+			map("[[", "prev")
+
+			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buffer = vim.api.nvim_get_current_buf()
+					map("]]", "next", buffer)
+					map("[[", "prev", buffer)
+				end,
+			})
+		end,
+		keys = {
+			{ "]]", desc = "Next Reference" },
+			{ "[[", desc = "Prev Reference" },
+		},
+	},
 }
