ed895bab6c7043452c2a87b3af06af79f21163ee
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-20 07:30:43 -0300

    Allow moving slected lines

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,9 +6,10 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.cmd("set undofile")
 vim.cmd("set undodir=~/.vim/undo")
-vim.cmd("set spelllang=en_us")
-vim.cmd("set spell")
+-- vim.cmd("set spelllang=en_us")
+-- vim.cmd("set spell")
 vim.cmd("set splitright")
+vim.cmd("set nowrap")
 
 -- Disable spelling for terminal windows
 vim.cmd("au TermOpen * setlocal nospell")
@@ -20,3 +21,8 @@ vim.g.mapleader = " "
 
 -- Sync clipboard with system
 vim.api.nvim_set_option("clipboard", "unnamed")
+
+vim.cmd("nnoremap K :m .-2<CR>==")
+vim.cmd("nnoremap J :m .+1<CR>==")
+vim.cmd("vnoremap K :m '<-2<CR>gv=gv")
+vim.cmd("vnoremap J :m '>+1<CR>gv=gv")
