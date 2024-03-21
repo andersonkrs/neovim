c73c03024b7dd81a2dcb202ffffa6a475e5aab1f
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-20 21:01:34 -0300

    Prettify git configs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,52 +1,52 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-    keys = {
-      { "<Leader>hp", "<cmd>Gitsigns preview_hunk<CR>",              mode = { "n", "v" }, desc = "Preview Hunk" },
-      { "<Leader>hb", "<cmd>Gitsigns toggle_current_line_blame<CR>", mode = { "n", "v" }, desc = "Toggle Blame" },
-      {
-        "]h",
-        "<cmd>Gitsigns next_hunk<CR>",
-        mode = { "n", "v" },
-        desc = "Move Next Hunk",
-      },
-      {
-        "[h",
-        "<cmd>Gitsigns prev_hunk<CR>",
-        mode = { "n", "v" },
-        desc = "Move Previous Hunk",
-      },
-    },
-  },
-  {
-    "tpope/vim-fugitive",
-  },
-  {
-    "tpope/vim-rhubarb",
-    keys = {
-      { "<Leader>go", "<cmd>.GBrowse<CR>",  mode = { "n", "v" }, desc = "Browse Code in Github" },
-      { "<Leader>gh", "<cmd>.GBrowse!<CR>", mode = { "n", "v" }, desc = "Copy Github Link" },
-    },
-  },
-  {
-    "kdheepak/lazygit.nvim",
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<Leader>gg", "<cmd>LazyGit<CR>", mode = { "n", "v" }, desc = "Open LazyGit" },
-    },
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+		event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+		keys = {
+			{ "<Leader>hp", "<cmd>Gitsigns preview_hunk<CR>", mode = { "n", "v" }, desc = "Preview Hunk" },
+			{ "<Leader>hb", "<cmd>Gitsigns toggle_current_line_blame<CR>", mode = { "n", "v" }, desc = "Toggle Blame" },
+			{
+				"]h",
+				"<cmd>Gitsigns next_hunk<CR>",
+				mode = { "n", "v" },
+				desc = "Move Next Hunk",
+			},
+			{
+				"[h",
+				"<cmd>Gitsigns prev_hunk<CR>",
+				mode = { "n", "v" },
+				desc = "Move Previous Hunk",
+			},
+		},
+	},
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"tpope/vim-rhubarb",
+		keys = {
+			{ "<Leader>go", "<cmd>.GBrowse<CR>", mode = { "n", "v" }, desc = "Browse Code in Github" },
+			{ "<Leader>gh", "<cmd>.GBrowse!<CR>", mode = { "n", "v" }, desc = "Copy Github" },
+		},
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		-- optional for floating window border decoration
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<Leader>gg", "<cmd>LazyGit<CR>", mode = { "n", "v" }, desc = "Open LazyGit" },
+		},
+	},
 }
