b00a78d8d173a0f1ddbb75f4d356b40dca9e679b
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-29 07:32:56 -0300

    Uses Other plugin to quickly navigate between files

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,75 +1,87 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"github/copilot.vim",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {},
-	},
-	{
-		"tpope/vim-projectionist",
-		config = function()
-			vim.g.projectionist_heuristics = {
-				["*"] = {
-					["lib/tasks/*.rake"] = {
-						alternate = "spec/lib/tasks/{}_spec.rb",
-					},
-					["spec/lib/tasks/*_spec.rb"] = {
-						alternate = "lib/tasks/{}.rake",
-					},
-					-- ["spec/factories/*.rb"] = {
-					-- 	type = "factory",
-					-- 	related = "models/{singular}.rb",
-					-- },
-				},
-			}
-		end,
-	},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" }, -- For luasnip users.
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {},
+  },
+  {
+    "rgroli/other.nvim",
+    config = function()
+      require("other-nvim").setup({
+        mappings = {
+          "rails",
+          {
+            pattern = "(.+)/packs/(.*)/spec/public/(.*)_spec.rb$",
+            target = "%1/packs/%2/app/public/%3.rb",
+          },
+          {
+            pattern = "(.+)/packs/(.*)/spec/(.*)_spec.rb$",
+            target = "%1/packs/%2/app/%3.rb",
+          },
+        },
+        style = {
+          -- How the plugin paints its window borders
+          -- Allowed values are none, single, double, rounded, solid and shadow
+          border = "solid",
+
+          -- Column seperator for the window
+          seperator = "|",
+
+          -- width of the window in percent. e.g. 0.5 is 50%, 1.0 is 100%
+          width = 0.7,
+
+          -- min height in rows.
+          -- when more columns are needed this value is extended automatically
+          minHeight = 2,
+        },
+      })
+      vim.api.nvim_set_keymap("n", "<C-A>", "<cmd>:Other<CR>", { noremap = true, silent = true })
+    end,
+  },
 }
--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,15 +1,14 @@
 return {
-  {
-    "vim-test/vim-test",
-    config = function()
-      vim.cmd("let test#strategy = 'neovim'")
-      vim.cmd("let test#neovim#term_position = 'vertical'")
+	{
+		"vim-test/vim-test",
+		config = function()
+			vim.cmd("let test#strategy = 'toggleterm'")
+			vim.cmd("let test#neovim#term_position = 'vertical'")
 
-
-      vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>", { desc = "Test Nearest" })
-      vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>", { desc = "[T]est [F]ile" })
-      vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "[T]est [L]ast" })
-      vim.keymap.set("n", "<leader>tg", "<cmd>TestVisit<CR>", { desc = "[T]est Visit" })
-    end,
-  }
+			vim.keymap.set("n", "<leader>tt", "<cmd>TestNearest<CR>", { desc = "Test Nearest" })
+			vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<CR>", { desc = "[T]est [F]ile" })
+			vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<CR>", { desc = "[T]est [L]ast" })
+			vim.keymap.set("n", "<leader>tg", "<cmd>TestVisit<CR>", { desc = "[T]est Visit" })
+		end,
+	},
 }
