1ecc6e1e0d57600e47fc667c961af4e688c79af7
Anderson Macedo <anderson.krs95@gmail.com>
2024-03-29 07:35:53 -0300

    Disable file alternation from vim-rails

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -6,8 +6,6 @@ return {
         pattern = "eruby.yaml",
         command = "set filetype=yaml",
       })
-
-      vim.keymap.set({ "n", "v" }, "<C-A>", "<cmd>A<CR>", { desc = "Alternate between rails files" })
     end,
   },
 }
